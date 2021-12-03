a355a6e0ff0c69086557c96265df42fdddda25e4
alin34 <april.lindsey@nike.com>
2021-12-02 22:37:37 -0800

    day 3

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -1,17 +1,76 @@
 object Day3 {
 
+  // needed for sample
+//  val SIZE = 5
+  // needed for real
+  val SIZE = 12
+
   /** ****** Day 3 Part 1 ********* */
   /**
    */
   def find1(in: Array[String]): Int = {
-    0
+    val gammaBinaryStr = (0 until SIZE).map(i => findGreatest(in, i))
+      .mkString("")
+    val epsilonBinaryStr = (0 until SIZE).map(i => findLeast(in, i))
+      .mkString("")
+    val gDecimalValue = Integer.parseInt(gammaBinaryStr, 2)
+    val eDecimalValue = Integer.parseInt(epsilonBinaryStr, 2)
+    eDecimalValue * gDecimalValue
+  }
+
+  def findGreatest(in: Array[String], positionInString: Int): String = {
+    val zeros = in.map(i => i.charAt(positionInString))
+      .count(i => i == '0')
+    val ones = in.map(i => i.charAt(positionInString))
+      .count(i => i == '1')
+    if (zeros > ones) "0" else "1"
+  }
+
+  def findLeast(in: Array[String], positionInString: Int): String = {
+    val zeros = in.map(i => i.charAt(positionInString))
+      .count(i => i == '0')
+    val ones = in.map(i => i.charAt(positionInString))
+      .count(i => i == '1')
+    if (zeros > ones) "1" else "0"
   }
 
   /** ****** Day 3 Part 2 ********* */
   /**
    */
   def find2(in: Array[String]): Int = {
-    0
+    val a = findStringWithGreatest(in)
+    val b = findStringWithLeast(in)
+
+    val aDecimalValue = Integer.parseInt(a, 2)
+    val bDecimalValue = Integer.parseInt(b, 2)
+
+    aDecimalValue * bDecimalValue
+  }
+
+  private def findStringWithGreatest(in: Array[String]): String = {
+    var answer = ""
+    var remainingStrings: Array[String] = in
+    (0 until SIZE)
+      .takeWhile(_ => answer.equals(""))
+      .foreach(a => {
+        val greatestBinaryStr = findGreatest(remainingStrings, a)
+        remainingStrings = Seq.from(remainingStrings).filter(_.charAt(a).equals(greatestBinaryStr.charAt(0))).toArray
+        if (remainingStrings.length == 1) answer = remainingStrings(0)
+      })
+    answer
+  }
+
+  private def findStringWithLeast(in: Array[String]): String = {
+    var answer = ""
+    var remainingStrings: Array[String] = in
+    (0 until SIZE)
+      .takeWhile(_ => answer.equals(""))
+      .foreach(a => {
+        val leastBinaryStr = findLeast(remainingStrings, a)
+        remainingStrings = Seq.from(remainingStrings).filter(_.charAt(a).equals(leastBinaryStr.charAt(0))).toArray
+        if (remainingStrings.length == 1) answer = remainingStrings(0)
+      })
+    answer
   }
 
 }
--- src/test/scala/AdventOfCodeSpec.scala
+++ src/test/scala/AdventOfCodeSpec.scala
@@ -12,29 +12,46 @@ class AdventOfCodeSpec extends UnitSpec {
     fileStr.split("\n")
   }
 
-  "Day1 Part 1 #1" should "return answer" in {
+  "Day1 Part  #1" should "return answer" in {
     val fileLines = readFileLines("input1.csv")
     println("Number of increasing values: " + Day1.findIncreasing(fileLines))
   }
 
   // tried 1856
-  "Day1 Part 1 #2" should "return answer" in {
+  "Day1 Part #2" should "return answer" in {
     val fileLines = readFileLines("input1.csv")
     println("Number of increasing sums: " + Day1.find3SumIncreasing(fileLines))
   }
 
-  "Day1 Part 1 #2 sample" should "return answer" in {
+  "Day1 Part #2 sample" should "return answer" in {
     val fileLines = readFileLines("sampleInput1.2.csv")
     println("Number of increasing sums: " + Day1.find3SumIncreasing(fileLines))
   }
 
-  "Day1 Part 2 #1" should "return answer" in {
+  "Day2 Part #1" should "return answer" in {
     val fileLines = readFileLines("input2.1.txt")
     println("Answer: " + Day2.findEnd3DLocation(fileLines))
   }
 
-  "Day1 Part 2 #2" should "return answer" in {
+  "Day2 Part #2" should "return answer" in {
     val fileLines = readFileLines("input2.1.txt")
     println("Answer: " + Day2.findEnd3DLocationWithAim(fileLines))
+  }
+
+  "Day3 Part #1" should "return answer" in {
+    val fileLines = readFileLines("input3.1.txt")
+    println("Answer: " + Day3.find1(fileLines))
+  }
+
+  // 738423 too low
+  "Day3 Part #2" should "return answer" in {
+    val fileLines = readFileLines("input3.1.txt")
+    println("Answer: " + Day3.find2(fileLines))
+  }
+
+  // 220 instead of 230
+  "Day3 Part #2 sample" should "return answer" in {
+    val fileLines = readFileLines("sampleInput3.1.txt")
+    println("Answer: " + Day3.find2(fileLines))
   }
 }
